97292664f53a09465ea5ea50855ce06e745ba8f8
Kelden Norbu <12200008.gcit@rub.edu.bt>
2023-04-14 12:33:58 +0600

    FINAL PROJECT

---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,90 @@
+import React, { Fragment, useState } from "react";
+
+const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description);
+
+  //edit description function
+
+  const updateDescription = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { description };
+      await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        // onClick={document.querySelector(".form-control").focus()}
+        data-bs-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
+
+      <div
+        className="modal fade"
+        id={`id${todo.todo_id}`}
+        onClick={() => setDescription(todo.description)}
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Edit
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+                onClick={(e) => updateDescription(e)}
+              >
+                update
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default EditTodo;
